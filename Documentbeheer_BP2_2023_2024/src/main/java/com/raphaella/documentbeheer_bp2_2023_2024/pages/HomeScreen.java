f1f919b36a0c5765dc404b6194ebbe92390e12dc
Rfgeijk <Rfgvaneijk@gmail.com>
2024-01-17 15:27:20 +0100

    Login en HomeScreen
    Ik heb een Loginfunctie aangemaakt in mijn "helloApplication" file en heb deze file ook een andere naam gegeven en hij heet nu "ApplicationLogin". Ik heb hierna een HomeScreen aangemaakt en heb ervoor gezorgd dat deze opent wanneer de Username en Password juist zijn. Ook heb ik de specifieke "Loginpagina" die ik eerder aangemaakt had verwijderd aangezien ik deze niet meer nodig had.

--- Documentbeheer_BP2_2023_2024/src/main/java/com/raphaella/documentbeheer_bp2_2023_2024/pages/HomeScreen.java
+++ Documentbeheer_BP2_2023_2024/src/main/java/com/raphaella/documentbeheer_bp2_2023_2024/pages/HomeScreen.java
@@ -1,4 +1,70 @@
 package com.raphaella.documentbeheer_bp2_2023_2024.pages;
 
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
 public class HomeScreen {
+
+    private Scene scene;
+
+    public HomeScreen() {
+
+
+        GridPane root = new GridPane();
+
+        FlowPane welcome = new FlowPane();
+        welcome.setPrefSize(300,70);
+        welcome.setStyle("-fx-background-color: #59D7BF");
+        welcome.setPadding(new Insets(20, 20, 20, 20));
+
+        Text welcomeText = new Text();
+        welcomeText.setText("Welcome! Search for documents or upload one down below!");
+        welcomeText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        welcomeText.setTextAlignment(TextAlignment.CENTER);
+
+        welcome.getChildren().add(welcomeText);
+
+        Button documentsPage = new Button("You can look for documents here!");
+        documentsPage.setOnAction(a->{
+            SearchPage zp = new SearchPage();
+        });
+
+
+        Button addDocument = new Button("Add document");
+        addDocument.setPrefSize(90,30);
+        addDocument.setPadding(new Insets(0, 0, 0, 0));
+
+        addDocument.setOnAction(a->{
+            UploadPage up = new UploadPage();
+            UploadPage.show();
+        });
+
+
+
+
+        root.add(welcome,0 ,0 );
+        root.add(documentsPage, 0, 1);
+        root.add(addDocument, 0, 2);
+        root.getChildren();
+
+        scene = new Scene(root, 800, 250);
+        Stage stage = new Stage();
+        stage.setTitle("Home Screen");
+        stage.setScene(getScene());
+        stage.show();
+
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public void show() {
+    }
 }
