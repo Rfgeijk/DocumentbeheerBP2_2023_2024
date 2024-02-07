1c2a73ed6c2115c7aab6b43b445f847443faab38
Rfgeijk <Rfgvaneijk@gmail.com>
2024-02-07 09:23:41 +0100

    Update JAR-file
    JAR-file toegevoegd aan het systeem

---
+++ Documentbeheer_BP2_2023_2024/src/main/java/com/raphaella/documentbeheer_bp2_2023_2024/classes/StartUp.java
@@ -0,0 +1,9 @@
+package com.raphaella.documentbeheer_bp2_2023_2024.classes;
+
+import com.raphaella.documentbeheer_bp2_2023_2024.ApplicationLogin;
+
+public class StartUp {
+    public static void main(String[] args) {
+        ApplicationLogin.main(args);
+    }
+}
